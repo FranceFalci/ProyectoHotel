d77691703fcd3dc5d8934642402eb9d8ff209333
Francesca <francescafacia@gmail.com>
2023-11-29 16:11:53 -0300

    clase huesped terminada

--- src/Huesped.cpp
+++ src/Huesped.cpp
@@ -7,9 +7,26 @@
 
 #include "Huesped.h"
 
-Huesped::Huesped() {
-	// TODO Auto-generated constructor stub
+Huesped::Huesped(string nombre, string dni) {
+	this->nombre = nombre;
+	this->dni = dni;
+}
+void Huesped::ListarInfo(){
+	cout << "Nombre:" <<this->nombre<< endl;
+	cout << "DNI:" <<this->dni<< endl;
+};
 
+Huesped::Huesped(string nombre, string apellido, int nroHabitacion, string dni, string nacionalidad, string provincia, string mail, string domicilio, string patente, string telefono) {
+    this->nombre = nombre;
+    this->apellido = apellido;
+    this->nroHabitacion = nroHabitacion;
+    this->dni = dni;
+    this->nacionalidad = nacionalidad;
+    this->provincia = provincia;
+    this->mail = mail;
+    this->domicilio = domicilio;
+    this->patente = patente;
+    this->telefono = telefono;
 }
 
 Huesped::~Huesped() {
