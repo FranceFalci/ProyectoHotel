b3d31542fc3817db59645f80bb5bd7acdc6943a5
“NatiRollan” <natyangela2010@gmail.com>
2023-11-29 16:58:48 -0300

    hotel

---
+++ src/Hotel.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hotel.cpp
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: HP
+ */
+
+#include "Hotel.h"
+
+Hotel::Hotel() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Hotel::~Hotel() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Hotel.h
@@ -0,0 +1,17 @@
+/*
+ * Hotel.h
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: HP
+ */
+
+#ifndef HOTEL_H_
+#define HOTEL_H_
+
+class Hotel {
+public:
+	Hotel();
+	virtual ~Hotel();
+};
+
+#endif /* HOTEL_H_ */
