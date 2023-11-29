fc55b08d9dd3cfe50d16d509dbd19579f7ff5c06
“NatiRollan” <natyangela2010@gmail.com>
2023-11-29 16:11:35 -0300

    amenities

---
+++ src/ReservaAmenities.cpp
@@ -0,0 +1,18 @@
+/*
+ * ReservaAmenities.cpp
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: HP
+ */
+
+#include "ReservaAmenities.h"
+
+ReservaAmenities::ReservaAmenities() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ReservaAmenities::~ReservaAmenities() {
+	// TODO Auto-generated destructor stub
+}
+
