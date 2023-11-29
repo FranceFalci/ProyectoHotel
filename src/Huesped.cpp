6656d0608087070ca4b323c3ebb0e664335b32d2
Francesca <francescafacia@gmail.com>
2023-11-29 15:56:20 -0300

    first commit

---
+++ src/Huesped.cpp
@@ -0,0 +1,18 @@
+/*
+ * Huesped.cpp
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: franc
+ */
+
+#include "Huesped.h"
+
+Huesped::Huesped() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Huesped::~Huesped() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Huesped.h
@@ -0,0 +1,17 @@
+/*
+ * Huesped.h
+ *
+ *  Created on: 29 nov. 2023
+ *      Author: franc
+ */
+
+#ifndef HUESPED_H_
+#define HUESPED_H_
+
+class Huesped {
+public:
+	Huesped();
+	virtual ~Huesped();
+};
+
+#endif /* HUESPED_H_ */
